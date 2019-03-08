e05d94b00ab831c810eea29234079f15743f5790
Hackbright Student <no-reply@hackbrightacademy.com>
2019-03-07 21:04:01 -0800

    finished until GRADE LISTING FOR STUDENT

--- hackbright_web.py
+++ hackbright_web.py
@@ -11,11 +11,44 @@ app = Flask(__name__)
 def get_student():
     """Show information about a student."""
 
-    github = "jhacks"
+    github = request.args.get('github')
 
     first, last, github = hackbright.get_student_by_github(github)
 
-    return "{} is the GitHub account for {} {}".format(github, first, last)
+    return render_template("student_info.html",
+                        github=github, 
+                        first=first, 
+                        last=last)
+
+@app.route("/student-search")
+def get_student_form():
+    """Show form searching for a student."""
+
+    return render_template("student_search.html")
+
+
+@app.route("/student-add")
+def student_add():
+    """Add a student."""
+
+    return render_template('new_student.html')
+
+@app.route("/student-added", methods=['POST'])
+def student_added():
+    """Add a student."""
+
+    first_name = request.form.get('first_name')
+    last_name = request.form.get('last_name')
+    github = request.form.get('github')
+
+    hackbright.make_new_student(first_name, last_name, github)
+
+    return render_template('student_added.html',
+                            first_name = first_name,
+                            last_name=last_name,
+                            github=github)
+
+    
 
 if __name__ == "__main__":
     hackbright.connect_to_db(app)
